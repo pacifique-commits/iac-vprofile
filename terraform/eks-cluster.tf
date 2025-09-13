036856a60fe530a5589ef4994c5e9df577247cbc
pacifique-commits <typaccy@gmail.com>
2025-09-13 12:15:36 +0200

    KMS

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -9,6 +9,10 @@ module "eks" {
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
+  # Disable KMS encryption to avoid permission issues
+  create_kms_key            = false
+  cluster_encryption_config = []
+
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
 
