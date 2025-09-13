b4ed3bce239d578990d63715ce243233cfbce7cb
pacifique-commits <typaccy@gmail.com>
2025-09-13 12:21:05 +0200

    Validate

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -10,15 +10,12 @@ module "eks" {
   cluster_endpoint_public_access = true
 
   # Force creation of new KMS key with proper permissions
-  create_kms_key = true
-  kms_key_description = "EKS cluster ${local.cluster_name} encryption key"
+  create_kms_key                  = true
+  kms_key_description             = "EKS cluster ${local.cluster_name} encryption key"
   kms_key_deletion_window_in_days = 7
-
-
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
